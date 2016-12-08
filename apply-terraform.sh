163cf879a0e5b439ebc83cb000e833f3a98e37bd
Konstantin Burtsev <konstantin.burtsev@altoros.com>
2016-12-08 18:15:42 +0300

    install only terraform

--- apply-terraform.sh
+++ apply-terraform.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 set -xe
 
-apt-get -qq update
-apt-get -qq install wget
-apt-get -qq install build-essential git unzip ruby-bundler zlib1g-dev libsqlite3-dev libssl-dev zlibc zlib1g-dev openssl libxslt-dev libxml2-dev libreadline6 libreadline6-dev ruby ruby-dev libxml2-dev libsqlite3-dev libxslt1-dev libpq-dev libmysqlclient-dev zlib1g-dev
-
-wget -c https://s3.amazonaws.com/bosh-init-artifacts/bosh-init-0.0.96-linux-amd64
-chmod +x bosh-init-0.0.96-linux-amd64
-mv bosh-init-0.0.96-linux-amd64 /usr/local/bin/bosh-init
-
-wget -c https://github.com/cloudfoundry-incubator/spiff/releases/download/v1.0.7/spiff_linux_amd64.zip
-unzip spiff_linux_amd64.zip
-rm spiff_linux_amd64.zip
-mv spiff /usr/local/bin/
-
-wget -c https://releases.hashicorp.com/terraform/0.7.3/terraform_0.7.3_linux_amd64.zip
-unzip terraform_0.7.3_linux_amd64.zip
-rm terraform_0.7.3_linux_amd64.zip
+wget -c https://releases.hashicorp.com/terraform/0.7.13/terraform_0.7.13_linux_amd64.zip
+unzip terraform_0.7.13_linux_amd64.zip
+rm terraform_0.7.13_linux_amd64.zip
 mv terraform* /usr/local/bin/
 
 #check for versions
-bosh-init -v
-spiff -v
 terraform -v
 
 # export pub_key=$pub_key
@@ -40,5 +25,5 @@ export TF_VAR_region=$region
 #export TF_VAR_pub_key=`pwd`/deploy-cf-on-aws/jumpbox.pub
 #export TF_VAR_pvt_key=`pwd`/deploy-cf-on-aws/jumpbox.pem
 
-cd cf-workspace/terraform/aws-resources-only
+cd cf-workspace/terraform/aws
 make apply
