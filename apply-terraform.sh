e75779ac768efddddc08e30a1f005dbb8699d9ff
Konstantin Burtsev <konstantin.burtsev@altoros.com>
2016-12-08 18:43:48 +0300

    added new variables

--- apply-terraform.sh
+++ apply-terraform.sh
@@ -26,8 +26,16 @@ terraform -v
 export TF_VAR_access_key=$aws_id
 export TF_VAR_secret_key=$aws_key
 export TF_VAR_region=$region
-#export TF_VAR_pub_key=`pwd`/deploy-cf-on-aws/jumpbox.pub
-#export TF_VAR_pvt_key=`pwd`/deploy-cf-on-aws/jumpbox.pem
+export TF_VAR_pub_key=`pwd`/deploy-cf-on-aws/jumpbox.pub
+export TF_VAR_pvt_key=`pwd`/deploy-cf-on-aws/jumpbox.pem
+export TF_VAR_ssl_cert=$ssl_cert
+export TF_VAR_system_domain=$system_domain
+export TF_VAR_cf_release=$cf_release
+export TF_VAR_cf_secret=$cf_secret
+export TF_VAR_rds_password=$rds_password
+export TF_VAR_altoros-ci_pvt_key=$altoros-ci_pvt_key
 
 cd cf-workspace/terraform/aws
 make apply
+sleep 60
+make destroy
