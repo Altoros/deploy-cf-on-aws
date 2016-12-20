a65e9f7bf4034b2f01cf6e873a129803b8aca6f4
Konstantin Burtsev <konstantin.burtsev@altoros.com>
2016-12-20 11:45:16 +0300

    working release v1

--- apply-terraform.sh
+++ apply-terraform.sh
@@ -3,8 +3,6 @@ set -xe
 
 apt-get update
 apt-get install -y wget git unzip make
-#apt-get install -y wget git unzip build-essential
-#apt-get install -y build-essential git unzip ruby-bundler zlib1g-dev libsqlite3-dev libssl-dev zlibc zlib1g-dev openssl libxslt-dev libxml2-dev libreadline6 libreadline6-dev ruby ruby-dev libxml2-dev libsqlite3-dev libxslt1-dev libpq-dev libmysqlclient-dev zlib1g-dev
 
 wget -c https://releases.hashicorp.com/terraform/0.7.13/terraform_0.7.13_linux_amd64.zip
 unzip terraform_0.7.13_linux_amd64.zip
@@ -13,16 +11,6 @@ mv terraform* /usr/local/bin/
 
 #check for versions
 terraform -v
-
-# export pub_key=$pub_key
-# export pvt_key=$pvt_key
-#
-# printenv $PUB_KEY > ~/.ssh/id_rsa_tf.pub
-# printenv $PVT_KEY > ~/.ssh/id_rsa_tf
-
-#echo $pub_key > id_rsa_tf.pub
-#echo $pvt_key > id_rsa_tf
-
 
 export TF_VAR_access_key=$aws_id
 export TF_VAR_secret_key=$aws_key
@@ -38,7 +26,6 @@ export TF_VAR_rds_password=$rds_password
 echo "altorosci_pvt_key = <<EOF" >> cf-workspace/terraform/aws/terraform.tfvars
 echo "${altorosci_pvt_key}" >> cf-workspace/terraform/aws/terraform.tfvars
 echo "EOF" >> cf-workspace/terraform/aws/terraform.tfvars
-#export TF_VAR_altorosci_pvt_key=/tmp/altorosci.pem
 
 cd cf-workspace/terraform/aws
 make apply
