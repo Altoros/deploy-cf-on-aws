52050f94de1446d51928cf03e08c28c8df5d5f2f
Konstantin Burtsev <konstantin.burtsev@altoros.com>
2017-02-02 17:02:21 +0300

    added spaces

--- apply-terraform.sh
+++ apply-terraform.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -xe
 
-echo "access_key=$aws_id" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "secret_key=$aws_key" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "region=$region" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "pub_key=`pwd`/deploy-cf-on-aws/jumpbox.pub" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "pvt_key=`pwd`/deploy-cf-on-aws/jumpbox.pem" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "ssl_cert=$ssl_cert" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "system_domain=$system_domain" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "cf_release=$cf_release" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "cf_secret=$cf_secret" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "rds_password=$rds_password" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "route53_zoneid=$route53_zoneid" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "access_key = $aws_id" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "secret_key = $aws_key" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "region = $region" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "pub_key = `pwd`/deploy-cf-on-aws/jumpbox.pub" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "pvt_key = `pwd`/deploy-cf-on-aws/jumpbox.pem" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "ssl_cert = $ssl_cert" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "system_domain = $system_domain" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "cf_release = $cf_release" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "cf_secret = $cf_secret" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "rds_password = $rds_password" >> cf-workspace/terraform/aws/terraform.tfvars
+echo "route53_zoneid = $route53_zoneid" >> cf-workspace/terraform/aws/terraform.tfvars
 
 echo "altorosci_pvt_key = <<EOF" >> cf-workspace/terraform/aws/terraform.tfvars
 echo "${altorosci_pvt_key}" >> cf-workspace/terraform/aws/terraform.tfvars
