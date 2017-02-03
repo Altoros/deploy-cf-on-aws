a0d408af33e48a64b2b8ea42d940f0afe669a84a
Konstantin Burtsev <konstantin.burtsev@altoros.com>
2017-02-03 12:10:55 +0300

    small changes

--- apply-terraform.sh
+++ apply-terraform.sh
@@ -3,9 +3,9 @@ set -xe
 
 export TF_VAR_access_key=$aws_id
 export TF_VAR_secret_key=$aws_key
+export TF_VAR_pub_key=`pwd`/deploy-cf-on-aws/jumpbox.pub
+export TF_VAR_pvt_key=`pwd`/deploy-cf-on-aws/jumpbox.pem
 echo "region = \"$region\"" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "pub_key = \"`pwd`/deploy-cf-on-aws/jumpbox.pub\"" >> cf-workspace/terraform/aws/terraform.tfvars
-echo "pvt_key = \"`pwd`/deploy-cf-on-aws/jumpbox.pem\"" >> cf-workspace/terraform/aws/terraform.tfvars
 echo "ssl_cert = \"$ssl_cert\"" >> cf-workspace/terraform/aws/terraform.tfvars
 echo "system_domain = \"$system_domain\"" >> cf-workspace/terraform/aws/terraform.tfvars
 echo "cf_release = \"$cf_release\"" >> cf-workspace/terraform/aws/terraform.tfvars
